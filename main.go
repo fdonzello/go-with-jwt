f73226f9599407a6afed4788cfab64e128ad3648
Jordan Rash <cjr@cjrash.com>
2018-06-04 16:46:07 -0400

    Updated project to work with Buffalo 0.11.1
    $: JWT_KEY_PATH=`pwd`/jwtRS256.key buffalo test
    INFO[0000] go test -p 1 go-with-jwt go-with-jwt/actions go-with-jwt/grifts
    ?       go-with-jwt     [no test files]
    ok      go-with-jwt/actions     0.178s
    ?       go-with-jwt/grifts      [no test files]

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import (
 	"log"
 
 	"go-with-jwt/actions"
-
-	"github.com/gobuffalo/envy"
 )
 
 func main() {
-	port := envy.Get("PORT", "3000")
 	app := actions.App()
-	log.Fatal(app.Start(port))
+	if err := app.Serve(); err != nil {
+		log.Fatal(err)
+	}
 }
